f50d4fc8b502b84798c70e6b13e9bdeb6703702c
Chase Brown <chase.brown@ucdenver.edu>
2019-05-05 16:54:41 -0600

    May5.500

--- DataBaseServer/ServerRecognize.py
+++ DataBaseServer/ServerRecognize.py
@@ -1,22 +1,28 @@
-from bson.binary import Binary
+# Senior Design Spring 2019
+#
+# Server with connection to DB
+# Listen for connections from clients. If connection requests create Thread.
+# Receives embeddings from clients. Checks embeddings vs DB for 'best' match
+# Returns to client the Motion embeddings and user_id if recognized
+#
 from DataBaseServer.ServerUpdate import connect
-from imutils import paths
 import numpy as np
-import cv2
-import os
 import pickle
-import pprint
-from pymongo import MongoClient
 import scipy.spatial.distance as distance
 import socket
 from threading import Thread
 
 
 def recognize_face(embedding, users):
+    """
+    Calculate distance measurements between incomming face embedding and Database Users
+    :param embedding: Array, Embedding from Client
+    :param users: DB Connection
+    :return: Array, Int, Array of useres motion embeddings along with the users ID.
+    """
     temp = 0.1
     user_id = None
     user_motion = None
-    dist = None
     # Loop over the database dictionary's ID and encodings.
     for u in users.find():
         ident = u['user_id']
@@ -25,6 +31,7 @@ def recognize_face(embedding, users):
 
         for i in range(len(k)):
             dist = distance_metric(k[i], embedding, 1)
+            # todo play with threshold once we get more data.
             if dist < 0.005:
                 if dist < temp:
                     temp = dist
@@ -32,13 +39,11 @@ def recognize_face(embedding, users):
                     user_motion = m
 
     if user_id is not None:
-        return user_motion  #user_id, user_motion, dist
+        return user_motion  # todo add user_id to data returned for client
     else:
         return 'nouser'
 
 
-# **Facenet distance metrics function https://github.com/davidsandberg/facenet/blob/master/src/facenet.py
-# todo test diference in cosine similarity vrs sum of the square difference
 def distance_metric(embeddings1, embeddings2, metric=0):
     """
     :param embeddings1: database embedding
@@ -57,6 +62,9 @@ def distance_metric(embeddings1, embeddings2, metric=0):
 
 
 class Server:
+    """
+    Threaded Server connections
+    """
     def __init__(self, host, port):
         # connection to mongodb
         self.client = connect('mongodb://localhost', 27017)
@@ -69,12 +77,21 @@ class Server:
         self.server.listen(5)
 
     def listen_for_clients(self):
+        """
+        Listen for clients attemping to connect. Create connection thread
+        :return: None
+        """
         while True:
             client, addr = self.server.accept()
             print('Connection from: ' + str(addr[0]) + ':' + str(addr[1]))
             Thread(target=self.handle_client, args=(client, addr)).start()
 
-    def handle_client(self, client_socket, address):
+    def handle_client(self, client_socket):
+        """
+        Handler for threaded client
+        :param client_socket: Socket, Connection between client and server
+        :return: None, Unless error then return False. else close connection
+        """
         while True:
             try:
                 # data_array = b""
