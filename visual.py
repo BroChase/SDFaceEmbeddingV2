4b25fb5293449e389a86f7b394559bdb2fc22df8
Chase Brown <chase.brown@ucdenver.edu>
2019-05-14 15:03:28 -0600

    Update514

--- visual.py
+++ visual.py
@@ -1,37 +1,62 @@
 # Senior Design Spring 2019
 # 3D plot at least 1 point from each user in the database
 
-# todo. Connect to database and get recognition embeddings and plot on 3D plane for demonstration of spatial distance
-# todo between users
 
-import numpy as np
-import pickle
-from sklearn.decomposition import PCA
+from DataBaseServer.ServerUpdate import connect
 import matplotlib.pyplot as plt
+import matplotlib.colors as pltc
 from mpl_toolkits import mplot3d
+import numpy as np
+import pandas as pd
+import pickle
+from random import sample
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+client = connect('mongodb://localhost', 27017)
+db = client.SeniorDesign
+users = db.users
 
 
-database = pickle.loads(open('./output/embeddings.pickle', 'rb').read())
-
-ak = list(database.values())
-
-x = ak[0]
-
-pca = PCA(n_components=3)
-pca.fit(x)
-q = pca.transform(x)
-qc = q[:10]
-qm = q[10:19]
-qn = q[19:]
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-# todo better iteration function for plotting colors based on user
-for i in range(len(q)):
-    zdata = q[i][0]
-    xdata = q[i][1]
-    ydata = q[i][2]
-    ax.scatter3D(xdata, ydata, zdata, c='r')
+def get_cmap(n, name='hsv'):
+    return plt.cm.get_cmap(name, n)
 
 
+emb = []
+id = []
+for u in users.find():
+    k = pickle.loads(u['recognize'])
+    for i in k:
+        emb.append(i)
+        id.append(u['user_id'])
+
+x_df = pd.DataFrame(emb)
+y_df = pd.DataFrame(id)
+c = np.array([i for i in id])
+
+# df = pd.concat([x_df, y_df], axis=1, sort=False)
+x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.20)
+
+
+neigh = KNeighborsClassifier()
+neigh.fit(x_train, y_train)
+
+y_pred = neigh.predict(x_test)
+print(accuracy_score(y_test, y_pred))
+cm = confusion_matrix(y_test, y_pred)
+classes = unique_labels(c)
+print(cm)
+
+fig, ax = plt.subplots()
+im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+ax.figure.colorbar(im, ax=ax)
+# We want to show all ticks...
+title = 'test'
+ax.set(xticks=np.arange(cm.shape[1]),yticks=np.arange(cm.shape[0]), xticklabels=classes, yticklabels=classes,
+       title=title, ylabel='True label', xlabel='Predicted label')
+plt.xticks(rotation='vertical')
 plt.show()
